c59408376380a917dcab2bdeab05cd969564ecd6
Diogo Afonso <dfdafonso@gmail.com>
2013-12-05 01:46:36 +0000

    Added config.ini

---
+++ inc/config.php
@@ -0,0 +1,10 @@
+<?php 
+//define("ROOT_FOLDER", "/var/www/html");
+define("ROOT_FOLDER", "/home/falua/psi-pl/psi-pl001/public_html");
+
+// define('REDIRECT_URL_PATH', '/');
+define('REDIRECT_URL_PATH', '/~psi-pl001/');
+
+//define('ORACLE_CONN_STRING', '//localhost');
+define('ORACLE_CONN_STRING', '//luna.di.fc.ul.pt/difcul.alunos.di.fc.ul.pt');
+?>
