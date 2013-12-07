72bdf1a48c5735c9b886fc3047ee551ed122c053
Diogo Afonso <dfdafonso@gmail.com>
2013-12-07 02:17:33 +0000

    last edits

--- inc/common/nav.php
+++ inc/common/nav.php
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<ul class="nav nav-pills pull-left">
-	<li><a href="index.php">Home</a></li>
-</ul>
-<ul class="nav nav-pills pull-right">
-	<?php if (!$userLoggedIn) { ?>
-	<li><a href="registo.php">Registo</a>
-	</li>
-	<li><a href="login.php">Login</a>
-	</li>
-	<?php } else { ?>
-	<ul class="nav nav-pills pull-right">
-		<li><a href="consultarPerfil.php">Olá <?=$_SESSION['user_name'];?>
-		</a></li>
-		<li><a href="historicoCompras.php">Consultar Compras</a>
-		</li>
-		<li><a href="?logout=1">Logout</a>
-		</li>
-	</ul>
-	<?php } ?>
-</ul>
-=======
 <div class="navbar">
     <div class="navbar-inner">
         <ul class="nav pull-left">
@@ -44,4 +22,3 @@
         </ul>
     </div>
 </div>
->>>>>>> 3c77929c1fd16b92d65fcec8f77e80dce60a01ef
